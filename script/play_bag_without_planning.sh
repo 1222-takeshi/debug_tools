98091134d10a8ef3209a0a97309a1a6031660193
tanaka3 <ttatcoder@outlook.jp>
2022-09-16 11:28:45 +0900

    feat: add play bag without planning
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>

---
+++ script/play_bag_without_planning.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BAG_NAME=$1
+PLANNING_TOPIC=$(ros2 bag info $BAG_NAME | awk '{print $2}' | grep planning | grep -v mission_planning)
+
+COMMAND_OPTION='--remap '
+for i in ${PLANNING_TOPIC[@]}; do
+  TARGET_PLANNING_TOPIC=$i
+  RENAMED_TARGET_PLANNING_TOPIC=$(echo $TARGET_PLANNING_TOPIC | sed 's/\/planning/\/tmp\/planning/g')
+  COMMAND_OPTION=$COMMAND_OPTION' '$TARGET_PLANNING_TOPIC':='$RENAMED_TARGET_PLANNING_TOPIC
+done
+
+ros2 bag play $BAG_NAME $COMMAND_OPTION --clock
